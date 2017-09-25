c5dc3457cd2a06e30208287031dd1a40974f2d86
Prashant Chamoli <prashantchamoli.165@gmail.com>
2017-09-24 17:18:50 -1200

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,48 @@
+var socket = io(); 
+function submitfunction(){
+  var from = $('#user').val();
+  var message = $('#m').val();
+  if(message != '') {
+  socket.emit('chatMessage', from, message);
+}
+$('#m').val('').focus();
+  return false;
+}
+function notifyTyping() { 
+  var user = $('#user').val();
+  socket.emit('notifyUser', user);
+}
+socket.on('chatMessage', function(from, msg){
+  var me = $('#user').val();
+  var color = (from == me) ? 'green' : '#009afd';
+  var from = (from == me) ? 'Me' : from;
+if(from=='Me')
+  $('#messages').append('<li style="text-align:right;"><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>');
+else
+$('#messages').append('<li><b style="color:' + color + '">' + from + '</b>: ' + msg + '</li>');
+
+
+});
+//function to notify other user when typing..
+socket.on('notifyUser', function(user){
+  var me = $('#user').val();
+  if(user != me) {
+    $('#notifyUser').text(user + ' is typing ...');
+  }
+  setTimeout(function(){ $('#notifyUser').text(''); }, 10000);;
+});
+$(document).ready(function(){
+  var name = makeid();
+  $('#user').val(name);
+  socket.emit('chatMessage', 'System', '<b>' + name + '</b> has joined the discussion');
+});
+
+// function to return a random username
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for( var i=0; i < 5; i++ ) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
